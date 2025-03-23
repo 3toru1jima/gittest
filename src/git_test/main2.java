39fb44bf2e33c0b1e1c82a8c6af9dee4705044ac
3toru1jima <62384658+3toru1jima@users.noreply.github.com>
2025-03-23 12:29:05 +0900

    Update main2.java

--- src/git_test/main2.java
+++ src/git_test/main2.java
@@ -4,7 +4,7 @@ public class main2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to git wolrd2");
+		System.out.println("welcome to git wolrd3");
 	}
 
 }
